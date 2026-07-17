d5934ffd59bd570627a00dec7d75884495d7c81e
nirupam-art <nirupamrajguru@gmail.com>
2026-07-17 10:43:33 +0530

    Updated website domain

--- app/robots.ts
+++ app/robots.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = "https://stans-school-swart.vercel.app";
+  const baseUrl = "https://www.stansjalore.in";
 
   return {
     rules: {
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://stans-school-swart.vercel.app";
+  const baseUrl = "https://www.stansjalore.in";
 
   return [
     {
